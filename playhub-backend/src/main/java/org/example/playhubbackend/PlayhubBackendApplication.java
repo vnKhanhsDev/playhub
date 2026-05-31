e8387dff3a2c4d251be65808146fbd806e88fa69
namkhanhs.dev <namkhanhs.dev@gmail.com>
2026-05-31 11:05:47 +0700

    chore: init project

---
+++ playhub-backend/src/main/java/org/example/playhubbackend/PlayhubBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.playhubbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayhubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayhubBackendApplication.class, args);
+    }
+
+}
---
+++ playhub-backend/src/test/java/org/example/playhubbackend/PlayhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.playhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayhubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
