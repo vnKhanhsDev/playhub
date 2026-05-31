a2d2de91c28f1eed33b87374257c1849ee689b6d
namkhanhs.dev <namkhanhs.dev@gmail.com>
2026-05-31 12:54:34 +0700

    chore: add security filter chain

--- playhub-backend/src/main/java/org/example/playhubbackend/config/SecurityConfig.java
+++ playhub-backend/src/main/java/org/example/playhubbackend/config/SecurityConfig.java
@@ -1,7 +1,31 @@
 package org.example.playhubbackend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
