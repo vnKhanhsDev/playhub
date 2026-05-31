b7913607254b18e3d0c0cf08d69c41ac5c49ff9a
namkhanhs.dev <namkhanhs.dev@gmail.com>
2026-05-31 12:09:04 +0700

    chore: add app exception handler

--- playhub-backend/src/main/java/org/example/playhubbackend/common/exception/AppException.java
+++ playhub-backend/src/main/java/org/example/playhubbackend/common/exception/AppException.java
@@ -1,20 +1,25 @@
 package org.example.playhubbackend.common.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+@Getter
 @RequiredArgsConstructor
 public class AppException extends RuntimeException {
 
     private final ErrorCode errorCode;
+    private final HttpServletRequest request;
 
     /*
     * @param errorCode: USER_NOT_FOUND
     * @param detail: "id=123"
     * @return: "User not found: id=123"
     * */
-    public AppException(ErrorCode errorCode, String detail) {
+    public AppException(ErrorCode errorCode, String detail, HttpServletRequest request) {
         super(errorCode.getMessage() + ": " + detail);
         this.errorCode = errorCode;
+        this.request = request;
     }
 
 }
