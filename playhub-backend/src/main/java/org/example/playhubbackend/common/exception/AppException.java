83ab55977c036a28c1c8adb7254cadc1c9004685
namkhanhs.dev <namkhanhs.dev@gmail.com>
2026-05-31 14:43:18 +0700

    chore: remove http request from app exception

--- playhub-backend/src/main/java/org/example/playhubbackend/common/exception/AppException.java
+++ playhub-backend/src/main/java/org/example/playhubbackend/common/exception/AppException.java
@@ -1,6 +1,5 @@
 package org.example.playhubbackend.common.exception;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,17 +8,15 @@ import lombok.RequiredArgsConstructor;
 public class AppException extends RuntimeException {
 
     private final ErrorCode errorCode;
-    private final HttpServletRequest request;
 
     /*
     * @param errorCode: USER_NOT_FOUND
     * @param detail: "id=123"
     * @return: "User not found: id=123"
     * */
-    public AppException(ErrorCode errorCode, String detail, HttpServletRequest request) {
+    public AppException(ErrorCode errorCode, String detail) {
         super(errorCode.getMessage() + ": " + detail);
         this.errorCode = errorCode;
-        this.request = request;
     }
 
 }
