d7704a25ed44046325685b3ee5345439fdfeaba6
namkhanhs.dev <namkhanhs.dev@gmail.com>
2026-06-02 10:40:39 +0700

    feat(user): optimize account registration and configure roles cascade

--- playhub-backend/src/main/java/org/example/playhubbackend/modules/user/service/AccountService.java
+++ playhub-backend/src/main/java/org/example/playhubbackend/modules/user/service/AccountService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.playhubbackend.common.exception.AppException;
 import org.example.playhubbackend.common.exception.ErrorCode;
 import org.example.playhubbackend.modules.user.entity.Account;
+import org.example.playhubbackend.modules.user.enums.AccountStatus;
 import org.example.playhubbackend.modules.user.enums.UserRole;
 import org.example.playhubbackend.modules.user.repository.AccountRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,26 +15,32 @@ import org.springframework.stereotype.Service;
 public class AccountService {
 
     private final AccountRepository accountRepository;
-
     private final PasswordEncoder passwordEncoder;
 
-    public void registerLocalAccount(String email, String password) {
-        if (accountRepository.existsByEmail(email)) {
-            throw new AppException(ErrorCode.ACCOUNT_EXISTED, "email: " + email);
+    public Account registerLocalAccount(String email, String password) {
+        Account account = accountRepository.findByEmail(email).orElse(null);
+
+        if (account != null) {
+            if (account.getStatus() != AccountStatus.PENDING_VERIFY) {
+                throw new AppException(ErrorCode.ACCOUNT_EXISTED, "email=" + email);
+            }
+            // Update password in case of registration retry with a different password
+            account.updatePassword(passwordEncoder.encode(password));
+            return accountRepository.save(account);
         }
 
-        createLocalAccount(email, password);
+        return createLocalAccount(email, password);
     }
 
-    public void createLocalAccount(String email, String password) {
-        Account newAccount = accountRepository.save(
-                Account.builder()
-                        .email(email)
-                        .passwordHash(passwordEncoder.encode(password))
-                        .build()
-        );
+    public Account createLocalAccount(String email, String password) {
+        Account newAccount = Account.builder()
+                .email(email)
+                .passwordHash(passwordEncoder.encode(password))
+                .build();
 
         newAccount.addRole(UserRole.PLAYER);
+
+        return accountRepository.save(newAccount);
     }
 
 }
